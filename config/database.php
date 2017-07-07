f036f212f5b9ab6797d0b5c406bfca8f99c013f8
Bruno Goncalves <bugotech@gmail.com>
2017-07-07 16:55:38 -0300

    MongoDB

--- config/database.php
+++ config/database.php
@@ -62,6 +62,18 @@ return [
             'engine' => null,
         ],
 
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'host'     => env('DB_HOST', 'localhost'),
+            'port'     => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'options'  => [
+                'database' => env('DB_DBNAME_AUTH', 'admin'), // sets the authentication database required by mongo 3
+            ]
+        ],
+
         'tests' => [
             'driver' => 'sqlite',
             'database' => ':memory:',
---
+++ src/MongoDbModel.php
@@ -0,0 +1,51 @@
+<?php namespace Bugotech\Db;
+
+use Bugotech\Db\Traits\CastModel;
+use Bugotech\Db\Traits\MutatorModel;
+use Bugotech\Db\Traits\LoaddingModel;
+use Bugotech\Db\Traits\ValidatorModel;
+use Bugotech\Db\Traits\DefaultValuesModel;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class MongoDbModel extends Eloquent
+{
+    use CastModel;
+    use MutatorModel;
+    use LoaddingModel;
+    use ValidatorModel;
+    use DefaultValuesModel;
+
+    protected $connection = 'mongodb';
+    public $timestamps = false;
+
+    /**
+     * Create a new Eloquent model instance.
+     *
+     * @param  array  $attributes
+     * @return void
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->fireModelEvent('loading', false);
+
+        parent::__construct($attributes);
+
+        $this->fireModelEvent('loaded', false);
+    }
+
+    /**
+     * Set a given attribute on the model.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return $this
+     */
+    public function setAttribute($key, $value)
+    {
+        // Tratar manipulaçãoes gerais
+        $value = $this->resolveMutator($key, $value);
+
+        // Atribuir valores
+        return parent::setAttribute($key, $value);
+    }
+}
